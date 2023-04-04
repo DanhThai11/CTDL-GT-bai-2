9fbdc2b26619d7a48a0c027fd6645b7cc7380a93
DanhThai11 <125196318+DanhThai11@users.noreply.github.com>
2023-04-04 16:13:08 +0700

    Update CTDL&GT_Interchange_Sort_2.cpp

--- CTDL&GT_Interchange_Sort_2.cpp
+++ CTDL&GT_Interchange_Sort_2.cpp
@@ -1,3 +1,6 @@
-input = 1874645 
-  mảng khi chưa sắp xếp là 1874645
-  mảng sau khi sắp xếp là 1445678
+input = {1,8,7,4,6}
+  mảng khi chưa sắp xếp là 1,8,7,4,6
+  b1 1,7,8,4,6
+  b2 1,4,7,8,6
+  b3 1,4,6,7,8
+ sau khi sắp xếp là 1,4,6,7,8
